a276816c4d6b1a3fe4c1da38ddb6c6465bb557df
mariobros123413 <j.mario18@hotmail.es>
2024-09-02 11:29:36 -0400

    Swiper Pagination

---
+++ lib/infraestructure/mappers/movie_mapper.dart
@@ -0,0 +1,26 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infraestructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB movieDB) => Movie(
+        adult: movieDB.adult,
+        backdropPath: (movieDB.backdropPath != '')
+            ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}'
+            : 'https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+        genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
+        id: movieDB.id,
+        originalLanguage: movieDB.originalLanguage,
+        originalTitle: movieDB.originalTitle,
+        overview: movieDB.overview,
+        popularity: movieDB.popularity,
+        posterPath: (movieDB.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500/${movieDB.posterPath}'
+            // : 'https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+            : 'no-poster',
+        releaseDate: movieDB.releaseDate,
+        title: movieDB.title,
+        video: movieDB.video,
+        voteAverage: movieDB.voteAverage,
+        voteCount: movieDB.voteCount,
+      );
+}
