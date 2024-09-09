3b3303941e9c2ce7ec7f132d9769b25b4e98edc1
mariobros123413 <j.mario18@hotmail.es>
2024-09-09 18:16:34 -0400

    Navigate en Search Sugestions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -49,7 +49,10 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
         final movies = snapshot.data ?? [];
         return ListView.builder(
           itemCount: movies.length,
-          itemBuilder: (context, index) => _MovieItem(movie: movies[index]),
+          itemBuilder: (context, index) => _MovieItem(
+            movie: movies[index],
+            onMovieSelected: close,
+          ),
         );
       },
     );
@@ -58,63 +61,69 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
 class _MovieItem extends StatelessWidget {
   final Movie movie;
-  const _MovieItem({required this.movie});
+  final Function onMovieSelected;
+  const _MovieItem({required this.movie, required this.onMovieSelected});
 
   @override
   Widget build(BuildContext context) {
     final textStyles = Theme.of(context).textTheme;
     final size = MediaQuery.of(context).size;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        children: [
-          //Image
-          SizedBox(
-            width: size.width * 0.2,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-                movie.posterPath,
-                loadingBuilder: (context, child, loadingProgress) => FadeIn(
-                  child: child,
+    return GestureDetector(
+      onTap: () {
+        onMovieSelected(context, movie);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+            //Image
+            SizedBox(
+              width: size.width * 0.2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  movie.posterPath,
+                  loadingBuilder: (context, child, loadingProgress) => FadeIn(
+                    child: child,
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          //Descripción
-          SizedBox(
-            width: size.width * 0.7,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  movie.title,
-                  style: textStyles.titleMedium,
-                ),
-                (movie.overview.length > 100)
-                    ? Text('${movie.overview.substring(0, 100)}..')
-                    : Text(movie.overview),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star_half_rounded,
-                      color: Colors.yellow.shade800,
-                    ),
-                    Text(
-                      HumanFormats.number(movie.voteAverage, 1),
-                      style: textStyles.bodyMedium!.copyWith(
+            const SizedBox(
+              width: 10,
+            ),
+            //Descripción
+            SizedBox(
+              width: size.width * 0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    movie.title,
+                    style: textStyles.titleMedium,
+                  ),
+                  (movie.overview.length > 100)
+                      ? Text('${movie.overview.substring(0, 100)}..')
+                      : Text(movie.overview),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star_half_rounded,
                         color: Colors.yellow.shade800,
                       ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+                      Text(
+                        HumanFormats.number(movie.voteAverage, 1),
+                        style: textStyles.bodyMedium!.copyWith(
+                          color: Colors.yellow.shade800,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
